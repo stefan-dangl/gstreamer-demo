d7c5ad66141acb6cda9a21d7c7cf8ad738ba8976
Stefan Dangl <s.dangl@viewpointsystem.com>
2026-03-08 19:19:36 +0100

    doc: Add Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Gstreamer Demo
+
+Ideas for demo project:
+- Waveform modulator
+    - GUI that allows to select different waveforms and audio properties. The waveform is shown graphically and the sound is outputted
+    - Based on Basic Tutorial 6, 7
+- [Gstreamer Rust tutorial code](https://gitlab.freedesktop.org/gstreamer/gstreamer-rs/-/blob/main/tutorials/src/bin/basic-tutorial-7.rs?ref_type=heads)
--- src/main.rs
+++ src/main.rs
@@ -2,144 +2,111 @@ use gst::{MessageType, prelude::*};
 
 fn tutorial_main() {
     // Initialize GStreamer
-    gst::init().unwrap();
+    if let Err(err) = gst::init() {
+        eprintln!("Failed to initialize Gst: {err}");
+        return;
+    }
 
-    let uri = "https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm";
-
-    // Create the elements
-    let source = gst::ElementFactory::make("uridecodebin")
-        .name("source")
-        // Set the URI to play
-        .property("uri", uri)
+    let audio_source = gst::ElementFactory::make("audiotestsrc")
+        .name("audio_source")
+        .property("freq", 215.0)
         .build()
-        .expect("Could not create uridecodebin element.");
+        .unwrap();
+    let tee = gst::ElementFactory::make("tee")
+        .name("tee")
+        .build()
+        .unwrap();
+    let audio_queue = gst::ElementFactory::make("queue")
+        .name("audio_queue")
+        .build()
+        .unwrap();
     let audio_convert = gst::ElementFactory::make("audioconvert")
-        .name("convert")
+        .name("audio_convert")
         .build()
-        .expect("Could not create convert element.");
+        .unwrap();
     let audio_resample = gst::ElementFactory::make("audioresample")
-        .name("resample")
+        .name("audio_resample")
         .build()
-        .expect("Could not create resample element.");
+        .unwrap();
     let audio_sink = gst::ElementFactory::make("autoaudiosink")
-        .name("sink")
+        .name("audio_sink")
         .build()
-        .expect("Could not create sink element.");
+        .unwrap();
+    let video_queue = gst::ElementFactory::make("queue")
+        .name("video_queue")
+        .build()
+        .unwrap();
+    let visual = gst::ElementFactory::make("wavescope")
+        .name("visual")
+        .property_from_str("shader", "none")
+        .property_from_str("style", "lines")
+        .build()
+        .unwrap();
     let video_convert = gst::ElementFactory::make("videoconvert")
         .name("video_convert")
         .build()
-        .expect("Could not create sink element.");
+        .unwrap();
     let video_sink = gst::ElementFactory::make("autovideosink")
         .name("video_sink")
         .build()
-        .expect("Could not create sink element.");
+        .unwrap();
 
-    // Create the empty pipeline
     let pipeline = gst::Pipeline::with_name("test-pipeline");
 
-    // Build the pipeline Note that we are NOT linking the source at this
-    // point. We will do it later.
     pipeline
         .add_many([
-            &source,
+            &audio_source,
+            &tee,
+            &audio_queue,
             &audio_convert,
             &audio_resample,
             &audio_sink,
+            &video_queue,
+            &visual,
             &video_convert,
             &video_sink,
         ])
         .unwrap();
-    gst::Element::link_many([&audio_convert, &audio_resample, &audio_sink])
-        .expect("Elements could not be linked.");
-    gst::Element::link_many([&video_convert, &video_sink]).expect("Elements could not be linked.");
 
-    // Connect the pad-added signal
-    source.connect_pad_added(move |src, src_pad| {
-        println!("Received new pad {} from {}", src_pad.name(), src.name());
+    gst::Element::link_many([&audio_source, &tee]).unwrap();
+    gst::Element::link_many([&audio_queue, &audio_convert, &audio_resample, &audio_sink]).unwrap();
+    gst::Element::link_many([&video_queue, &visual, &video_convert, &video_sink]).unwrap();
 
-        src.downcast_ref::<gst::Bin>()
-            .unwrap()
-            .debug_to_dot_file_with_ts(gst::DebugGraphDetails::all(), "pad-added");
+    let tee_audio_pad = tee.request_pad_simple("src_%u").unwrap();
+    println!(
+        "Obtained request pad {} for audio branch",
+        tee_audio_pad.name()
+    );
+    let queue_audio_pad = audio_queue.static_pad("sink").unwrap();
+    tee_audio_pad.link(&queue_audio_pad).unwrap();
 
-        let new_pad_caps = src_pad
-            .current_caps()
-            .expect("Failed to get caps of new pad.");
-        let new_pad_struct = new_pad_caps
-            .structure(0)
-            .expect("Failed to get first structure of caps.");
-        let new_pad_type = new_pad_struct.name();
+    let tee_video_pad = tee.request_pad_simple("src_%u").unwrap();
+    println!(
+        "Obtained request pad {} for video branch",
+        tee_video_pad.name()
+    );
+    let queue_video_pad = video_queue.static_pad("sink").unwrap();
+    tee_video_pad.link(&queue_video_pad).unwrap();
 
-        let is_audio = new_pad_type.starts_with("audio/x-raw");
-        let is_video = new_pad_type.starts_with("video/x-raw");
-
-        if is_audio {
-            let audio_sink_pad = audio_convert
-                .static_pad("sink")
-                .expect("Failed to get static sink pad from convert");
-            if audio_sink_pad.is_linked() {
-                println!("We are already linked. Ignoring.");
-                return;
-            }
-
-            if src_pad.link(&audio_sink_pad).is_err() {
-                println!("Type is {new_pad_type} but link failed.");
-            } else {
-                println!("Link succeeded (type {new_pad_type}).");
-            }
-        }
-
-        if is_video {
-            let video_sink_pad = video_convert
-                .static_pad("sink")
-                .expect("Failed to get static sink pad from convert");
-            if video_sink_pad.is_linked() {
-                println!("We are already linked. Ignoring.");
-                return;
-            }
-
-            if src_pad.link(&video_sink_pad).is_err() {
-                println!("Type is {new_pad_type} but link failed.");
-            } else {
-                println!("Link succeeded (type {new_pad_type}).");
-            }
-        }
-    });
-
-    // Start playing
     pipeline
         .set_state(gst::State::Playing)
         .expect("Unable to set the pipeline to the `Playing` state");
-
-    // Wait until State Change, error, or EOS
     let bus = pipeline.bus().unwrap();
     for msg in bus.iter_timed_filtered(
         gst::ClockTime::NONE,
-        &[
-            MessageType::StateChanged,
-            MessageType::Error,
-            MessageType::Eos,
-        ],
+        &[MessageType::Error, MessageType::Eos],
     ) {
         use gst::MessageView;
 
         match msg.view() {
             MessageView::Error(err) => {
                 eprintln!(
-                    "Error received from element {:?} {}",
+                    "Error received from element {:?}: {}",
                     err.src().map(|s| s.path_string()),
                     err.error()
                 );
                 eprintln!("Debugging information: {:?}", err.debug());
                 break;
-            }
-            MessageView::StateChanged(state_changed) => {
-                if state_changed.src().map(|s| s == &pipeline).unwrap_or(false) {
-                    println!(
-                        "Pipeline state changed from {:?} to {:?}",
-                        state_changed.old(),
-                        state_changed.current()
-                    );
-                }
             }
             MessageView::Eos(..) => break,
             _ => (),
